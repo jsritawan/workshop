f78e455e14fa98213adeea3a8a3738db8da6ddf9
ittikorn chawkamud <aseaways1234@gmail.com>
2023-01-22 00:27:46 +0700

    refactor: move struct pocket to cloudpocket

--- cloudpocket/cloudpocket.go
+++ cloudpocket/cloudpocket.go
@@ -8,6 +8,17 @@ type handler struct {
 	db *sql.DB
 }
 
+type Model struct {
+	ID          int64   `json:"id"`
+	Name        string  `json:"name"`
+	Budget      float64 `json:"budget"`
+	Balance     float64 `json:"balance"`
+	IsDefault   bool    `json:"isDefault"`
+	Description string  `json:"description"`
+	Current     string  `json:"current"`
+	AccountId   int64   `json:"accountId"`
+}
+
 func New(db *sql.DB) *handler {
 	return &handler{db}
 }
@@ -15,7 +26,4 @@ func New(db *sql.DB) *handler {
 const (
 	cStmtUpdateBalance = "UPDATE cloud_pockets SET balance = $1 WHERE id = $2;"
 	cStmtGetBalance    = "SELECT balance FROM cloud_pockets WHERE id = $1;"
-
-	cStmtUpdateBalanceTest = "UPDATE `cloud_pockets` SET (.+) WHERE (.+)"
-	cStmtGetBalanceTest    = "SELECT `balance` FROM `cloud_pockets` WHERE (.+)"
 )
