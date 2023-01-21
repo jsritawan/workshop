b95e819bb4241b7aedc99b2170692e55c9484a00
jeerapat <jeerapat@mfec.co.th>
2023-01-21 14:21:44 +0700

    update 02-init.sql

--- db/02-init.sql
+++ db/02-init.sql
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS "cloud_pockets" (
   "name" varchar NOT NULL,
   "budget" float8 NOT NULL DEFAULT 0,
   "balance" float8 NOT NULL DEFAULT 0,
-  "is_default" boolean,
-  "description" varchar,
+  "is_default" boolean DEFAULT false,
+  "description" varchar NOT NULL DEFAULT '',
   "currency" varchar NOT NULL,
-  "account_id" int4,
+  "account_id" int4 NOT NULL,
   PRIMARY KEY ("id")
 );
 
@@ -18,5 +18,3 @@ CREATE TABLE IF NOT EXISTS "accounts" (
   "balance" float8 NOT NULL DEFAULT 0,
   PRIMARY KEY ("id")
 );
-
-ALTER TABLE "cloud_pockets" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
