9bb6a324f48265844acf4c2a13f3fff0e21b0e18
ittikorn chawkamud <aseaways1234@gmail.com>
2023-01-21 12:53:27 +0700

    feat: add struct cloudpocket

---
+++ cloudpocket/cloudpocket_model.go
@@ -0,0 +1,12 @@
+package cloudpocket
+
+type CloudPocket struct {
+	ID          int64   `json:"id"`
+	Name        string  `json:"name"`
+	Budget      float64 `json:"budget"`
+	Balance     float64 `json:"balance"`
+	IsDefault   bool    `json:"isDefault"`
+	Description string  `json:"description"`
+	Current     string  `json:"current"`
+	AccountId   int64   `json:"accountId"`
+}
